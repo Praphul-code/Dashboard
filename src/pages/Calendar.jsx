fc9733e899fa7ecc514f8c43aa28b7908b34ee12
Praphul-whiz <arun@whiz-cloud.com>
2023-02-12 17:39:56 +0530

    setup-done

---
+++ src/pages/Calendar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Calendar = () => {
+  return (
+    <div>Claendar</div>
+  )
+}
+
+export default Calendar
---
+++ src/pages/Charts/ColorMapping.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ColorMappping = () => {
+  return (
+    <div>ColorMappping</div>
+  )
+}
+
+export default ColorMappping
